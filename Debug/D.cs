f256d4337ce4fcea9471ca66862a1d05747054ed
James Kim <eodnjs2998@gmail.com>
2015-01-10 19:19:02 +0900

    D 추가. Assert 지원.

---
+++ Debug/D.cs
@@ -0,0 +1,15 @@
+﻿#if UNITY_EDITOR
+using System;
+
+namespace Gem
+{
+	public static class D
+	{
+		public static void Assert(bool _val)
+		{
+			throw new Exception("assertion failed.");
+		}
+	}
+}
+
+#endif
---
+++ Debug/D_NDEBUG.cs
@@ -0,0 +1,13 @@
+﻿#if !UNITY_EDITOR
+using System.Diagnostics;
+
+namespace Gem
+{
+	public static class D
+	{
+		[Conditional("UNITY_EDITOR")]
+		public static void Assert(bool _val) {}
+	}
+}
+
+#endif
