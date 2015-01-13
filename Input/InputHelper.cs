5d4ada571a995689c02cb777477548fb4315aec3
James Kim <eodnjs2998@gmail.com>
2015-01-13 23:29:21 +0900

    DecorateDirection 추가.

--- Input/InputHelper.cs
+++ Input/InputHelper.cs
@@ -1,4 +1,4 @@
-﻿using LitJson;
+﻿using System;
 using UnityEngine;
 
 namespace Gem.In
@@ -15,6 +15,23 @@ namespace Gem.In
 		public static Direction GetDirection(float _deadzone = DirectionHelper.DEADZONE_DEFAULT)
 		{
 			return GetAxis().ToDirection(_deadzone);
+		}
+
+		public static void DecorateDirection(
+			this InputGroup _group, 
+			Func<Direction, bool> _down, 
+			Action<Direction> _update)
+		{
+			foreach (var _dir in EnumHelper.GetValues<Direction>())
+			{
+				var _dirCpy = _dir;
+				var _handler = new InputHandler { down = () => _down(_dirCpy) };
+				if (_handler.listen = (_update != null))
+					_handler.update = () => _update(_dirCpy);
+				_group.Add(_dir.ToInputCode(), _handler);
+			}
+
+			_group.Reg();
 		}
 	}
 
