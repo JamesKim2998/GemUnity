96ca0faccc4cd1c6a33be0da09de3af43b333d7c
James Kim <eodnjs2998@gmail.com>
2015-01-25 17:19:44 +0900

    Prefab added.

---
+++ Unity/Prefab.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Gem
+{
+	public struct Prefab
+	{
+		public Prefab(GameObject _go)
+		{
+			mGO = _go;
+		}
+
+		public GameObject Instantiate()
+		{
+			return mGO.Instantiate();
+		}
+
+		private readonly GameObject mGO;
+	}
+
+	public struct Prefab<T> where T: Component
+	{
+		public Prefab(T _go)
+		{
+			mGO = _go;
+		}
+
+		public T Instantiate()
+		{
+			return mGO.Instantiate();
+		}
+
+		private readonly T mGO;
+	}
+
+}
