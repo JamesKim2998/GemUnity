05647aa014d08f4f4cef0b15941f1999a2d728c6
James Kim <eodnjs2998@gmail.com>
2015-02-12 23:15:02 +0900

    ComponentHelper AddComponent 메소드 추가. AssetHelper 추가.

---
+++ Helper/AssetHelper.cs
@@ -0,0 +1,18 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+
+namespace Gem
+{
+	public static class Asset
+	{
+		public static T Load<T>(FullPath _path) where T : class
+		{
+			var _ret = AssetDatabase.LoadAssetAtPath(_path, typeof (T)) as T;
+			if (_ret == null) L.W(L.M.RSC_NOT_EXISTS(_path));
+			return _ret;
+		}
+	}
+}
+
+#endif
--- Helper/ComponentHelper.cs
+++ Helper/ComponentHelper.cs
@@ -4,20 +4,25 @@ namespace Gem
 {
 	public static class ComponentHelper
 	{
-		public static GameObject Instantiate(this GameObject _go)
+		public static GameObject Instantiate(this GameObject _this)
 		{
-			return (GameObject) Object.Instantiate(_go);
+			return (GameObject) Object.Instantiate(_this);
 		}
 
-		public static T Instantiate<T>(this T _comp) where T : Component
+		public static T Instantiate<T>(this T _this) where T : Component
 		{
-			return _comp.gameObject.Instantiate().GetComponent<T>();
+			return _this.gameObject.Instantiate().GetComponent<T>();
 		}
 
-		public static T AddIfNotExists<T>(this GameObject _go) where T : Component
+		public static T AddComponent<T>(this Component _this) where T : Component
 		{
-			var _comp = _go.GetComponent<T>();
-			return _comp ?? _go.AddComponent<T>();
+			return _this.gameObject.AddComponent<T>();
+		}
+
+		public static T AddIfNotExists<T>(this GameObject _this) where T : Component
+		{
+			var _comp = _this.GetComponent<T>();
+			return _comp ?? _this.AddComponent<T>();
 		}
 	}
 }
