8c5ea26c8f1688586a8843161ad8df2624a833bf
James Kim <eodnjs2998@gmail.com>
2015-02-15 12:07:38 +0900

    TKHelper 추가.

---
+++ Helper/TKHelper.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+
+namespace Gem
+{
+	public static class TKHelper 
+	{
+#if UNITY_EDITOR
+		public static tk2dSpriteCollection AssetCollection(FullPath _path)
+		{
+			var _anim = Asset.Load<GameObject>(_path);
+			return _anim ? _anim.GetComponent<tk2dSpriteCollection>() : null;
+		}
+
+		public static tk2dSpriteAnimation AssetAnimation(FullPath _path)
+		{
+			var _anim = Asset.Load<GameObject>(_path);
+			return _anim ? _anim.GetComponent<tk2dSpriteAnimation>() : null;
+		}
+#endif
+	}
+
+
+}
