27486f7d2f16cf1800f5d1ef0316e3cfcc099c7c
James Kim <eodnjs2998@gmail.com>
2015-02-18 02:26:04 +0900

    Pair added.

---
+++ DS/Pair.cs
@@ -0,0 +1,14 @@
+﻿namespace Gem
+{
+	public struct Pair<F, S>
+	{
+		public Pair(F _first, S _second)
+		{
+			first = _first;
+			second = _second;
+		}
+
+		public F first;
+		public S second;
+	}
+}
