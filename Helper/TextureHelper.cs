cacfc4780cb09dc2945ead4344e6c5568e2275fc
James Kim <eodnjs2998@gmail.com>
2015-02-20 17:12:32 +0900

    null 체크.

--- Helper/TextureHelper.cs
+++ Helper/TextureHelper.cs
@@ -11,6 +11,7 @@ namespace Gem
 
 		public static Sprite CreateSpite(this Texture2D _this, Vector2 _pivot)
 		{
+			if (!_this) return null;
 			return Sprite.Create(_this, new Rect(0, 0, _this.width, _this.height), _pivot);
 		}
 	}
