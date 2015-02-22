08b3019eac6a8840de46374eccfefa3e63be5fce
James Kim <eodnjs2998@gmail.com>
2015-02-22 09:06:12 +0900

    Image -> Graphic.

--- Helper/UIHelper.cs
+++ Helper/UIHelper.cs
@@ -4,28 +4,28 @@ namespace Gem
 {
 	public static class UIHelper 
 	{
-		public static void SetA(this Image _this, float _val)
+		public static void SetA(this Graphic _this, float _val)
 		{
 			var _c = _this.color;
 			_c.a = _val;
 			_this.color = _c;
 		}
 
-		public static void SetR(this Image _this, float _val)
+		public static void SetR(this Graphic _this, float _val)
 		{
 			var _c = _this.color;
 			_c.r = _val;
 			_this.color = _c;
 		}
 
-		public static void SetG(this Image _this, float _val)
+		public static void SetG(this Graphic _this, float _val)
 		{
 			var _c = _this.color;
 			_c.g = _val;
 			_this.color = _c;
 		}
 
-		public static void SetB(this Image _this, float _val)
+		public static void SetB(this Graphic _this, float _val)
 		{
 			var _c = _this.color;
 			_c.b = _val;
